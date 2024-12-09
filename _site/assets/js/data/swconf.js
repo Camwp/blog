66665eacdaad804645f8d7e778daff1a638ecbf2
CWP <77127867+Camwp@users.noreply.github.com>
2024-12-09 11:05:11 -0800

    favicon

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1733769647',resources: [
+    cacheName: 'chirpy-1733771076',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
